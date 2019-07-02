e19f8e81168cc1acc20cdaf0b8856320ed0fd63c
Eugene <eugenezh@123software.ru>
2019-07-02 11:29:00 +0300

    fix expire keys in get forecasts

--- utilities/redis/redisGetter.js
+++ utilities/redis/redisGetter.js
@@ -4,9 +4,11 @@ const getForecasts = async ( data ) => {
     const res = [];
     const keys = await forecasts.keysAsync(`charts_cache:${data.name}/*`);
     for (const key of keys) {
-        const forecast = JSON.parse(await forecasts.getAsync(key));
-        if(forecast.security === data.currency || !data.currency){
-            res.push(forecast);
+        if(!key.includes(':expire:')){
+            const forecast = JSON.parse(await forecasts.getAsync(key));
+            if(forecast.security === data.currency || !data.currency){
+                res.push(forecast);
+            }
         }
     }
     return res;
