a893f2530e9f5ef26c6a8f8a09d1f54c82fee1cc
Maxim Oancha <maksimoancha12@gmail.com>
2019-07-02 11:00:14 +0300

    change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,23 +8,32 @@ module.exports = {
         watch: false,
         max_memory_restart: '1G',
         env: {
-            NODE_ENV: 'production'
+            PORT: '8091'
         },
         env_production: {
-            NODE_ENV: 'production',
-            PORT: '8091'
+            NODE_ENV: 'production'
+        },
+        env_staging: {
+            NODE_ENV: 'staging'
         }
     }],
 
     deploy : {
         production : {
-            key: '/home/eugene/.ssh/waivio.pem',
+            user : 'admin',
+            host : '157.230.93.18',
+            ref  : 'origin/master',
+            repo : 'git@github.com:Waiviogit/investarena-api.git',
+            path : '/home/admin/investarena-api',
+            'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+        },
+		staging : {
             user : 'admin',
             host : '35.157.207.192',
             ref  : 'origin/master',
             repo : 'git@github.com:Waiviogit/investarena-api.git',
             path : '/home/admin/investarena-api',
-            'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+            'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env staging'
         }
     }
 };
