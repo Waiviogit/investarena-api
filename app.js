89ff9e2f43d39be2745fa390eed36a5aa5dc8369
Maxim Oancha <maksimoancha12@gmail.com>
2019-09-16 11:09:20 +0300

    add cors
    cors enabled

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const logger = require('morgan');
 const { routes } = require('./routes');
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use( cors() );
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
