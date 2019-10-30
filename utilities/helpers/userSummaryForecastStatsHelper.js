5efc2175a6b059f8a2fc9299039d791fff01aea6
Maxim Oancha <maksimoancha12@gmail.com>
2019-10-30 11:43:44 +0200

    add route to get user forecast stats by instruments
    request with params skip, limit, user name, sort field, sort direction, and return array of objects with keys quote, count and pips

--- utilities/helpers/userSummaryForecastStatsHelper.js
+++ utilities/helpers/userSummaryForecastStatsHelper.js
@@ -10,4 +10,27 @@ const getUserSummaryStats = async ({ name }) => {
     }
 };
 
-module.exports = { getUserSummaryStats };
+const getStatsByInstruments = async ({ name, limit, skip, sortBy, sortDirection }) => {
+    const { result, error } = await Forecast.fromAggregation(getStatsByInstrumentPipeline({ name, limit, skip, sortBy, sortDirection }));
+    if(error) return { error };
+    return{ result };
+};
+
+const getStatsByInstrumentPipeline = ({ name, limit, skip, sortBy, sortDirection }) => {
+    return [
+        { $match: { author: name } },
+        {
+            $group: {
+                _id: '$expForecast.rate.quote.security',
+                count: { $sum: 1 },
+                pips: { $sum: '$expForecast.profitability' }
+            }
+        },
+        { $project: { _id: 0, quote: '$_id', count: 1, pips: 1 } },
+        { $sort: { [ sortBy ]: sortDirection } },
+        { $skip: skip },
+        { $limit: limit }
+    ];
+};
+
+module.exports = { getUserSummaryStats, getStatsByInstruments };
